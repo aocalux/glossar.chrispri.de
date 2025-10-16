df49c34cb2e0be6e652dcf3170f9da1c737c724a
Chris Prickartz <cp@chris-prickartz.de>
2025-10-16 14:25:01 +0000

    add system

---
+++ script/popup.js
@@ -0,0 +1,31 @@
+function openSymbolerklaerung() {
+    window.open(
+        '/popup/symbolerklaerung.html',
+        'Symbolerklaerung',
+        'width=800,height=600,resizable=no,scrollbars=yes'
+    );
+}
+
+function openHowtosearch() {
+    window.open(
+        '/popup/howtosearch.html',
+        'How to Search',
+        'width=800,height=600,resizable=no,scrollbars=yes'
+    );
+}
+
+function openAbout() {
+    window.open(
+        '/popup/about.html',
+        'Als Quelle angeben',
+        'width=800,height=600,resizable=no,scrollbars=yes'
+    );
+}
+
+function openImpressumDatenschutz() {
+    window.open(
+        '/popup/impressumdatenschutz.html',
+        'Impressum & Datenschutz',
+        'width=800,height=600,resizable=no,scrollbars=yes'
+    );
+}
